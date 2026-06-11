00baaa38c185be131bd26a289f1c89bd93174c80
pachuau10 <duhteapachuau10@gmail.com>
2026-06-11 12:48:01 +0530

    force sslmode=require for supabase connection

--- chhohreivung/settings.py
+++ chhohreivung/settings.py
@@ -79,6 +79,7 @@ DATABASE_URL = config("DATABASE_URL", default="")
 if DATABASE_URL.startswith("postgres"):
     import dj_database_url
     DATABASES = {"default": dj_database_url.config(default=DATABASE_URL, conn_max_age=600)}
+    DATABASES["default"]["OPTIONS"] = {"sslmode": "require"}
 else:
     DATABASES = {
         "default": {
